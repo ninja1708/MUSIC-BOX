85ada087defe516a461b6cc9134505ce890fdd00
ninja1708 <133958245+ninja1708@users.noreply.github.com>
2024-11-15 12:18:52 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -36,11 +36,8 @@ python3.10 -m pip install discord.py yt_dlp
 echo "Instalowanie ffmepg"
 apt install ffmpeg
 
-echo "Instalacja Git"
-apt update && sudo apt install -y git
-
-echo "pobranie repozytorum"
-git clone https://github.com/ninja1708/MUSIC-BOX.git
+echo "Instalacja upadate"
+apt update
 
 
 
